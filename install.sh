39e9d1031bd1d71a9e6e78aad4e64025426698d5
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-10-30 13:11:30 +0300

    Improve install script (it's still crappy)

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+set -xefu
+dir0="$PWD"
+sudo mkdir -p /usr/share/microem
 echo "Install depends"
-sudo urpmi gcc-c++
-sudo urpmi libxdotool-devel
-sudo urpmi xdotool
-sudo sed -i "s/*class/*_class/g" /usr/include/xdo.h 
-mkdir distrib
+# apt install xdo-dev xdotool
+sudo urpmi gcc-c++ "$(rpm --eval '%_lib')xdotool-devel" xdotool || :
+# Что это и зачем?!
+#sudo sed -i "s/*class/*_class/g" /usr/include/xdo.h 
 echo "Build driver"
-PATH=$PATH:/sbin/
-cd driver
+cd "$dir0/driver"
 make
-sudo rmmod microem >/dev/null 2>&1
-sudo cp microem.ko /lib/modules/`uname -r`
-cp microem.ko ../distrib/
+sudo rmmod microem >/dev/null 2>&1 || :
+sudo mv -v microem.ko /lib/modules/`uname -r`
 sudo insmod /lib/modules/`uname -r`/microem.ko
-cd ..
+echo "microem" | sudo tee /etc/modprobe.d/microem.conf
+make clean
 echo "Build scanner"
-cd scanner
+cd "$dir0/scanner"
 make
-cp microemscan ../distrib/
-touch ../distrib/dont_send_eol.txt
+sudo install -m0755 microemscan /usr/local/bin/microemscan
+touch /usr/share/microem/dont_send_eol.txt
+sudo install -m0644 "$dir0/microem.service" /etc/systemd/system/microem.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now microem.service
